8de6cf70ede4f729d1c27ae01a4ab9ee7579563a
Salvo-P96 <salvop.6896@gmail.com>
2025-03-20 11:41:00 +0100

    Navbar added

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { ButtonRackComponent } from '../../components/button-rack/button-rack.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { ItemSliderComponent } from '../../components/item-slider/item-slider.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-main',
   imports: [ButtonRackComponent,
             CarouselComponent,
-            ItemSliderComponent
+            ItemSliderComponent,
+            NavbarComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
