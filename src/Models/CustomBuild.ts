d5c7d431cd94e4753c2b476619fceaad06ac92e9
Salvo-P96 <salvop.6896@gmail.com>
2025-03-25 13:11:31 +0100

    customcar class created

---
+++ src/Models/CustomBuild.ts
@@ -0,0 +1,26 @@
+export class Customcar{
+    public brand?:string;
+    public model?:string;
+    public configuration?:string;
+    public fuel?:string;
+    public setup?:string;
+    
+    
+    public color?:string;
+    public finish?:string;
+    public rimType?:string;
+    public rimSize?:number;
+    
+
+    public seatMaterial?:string;
+    public seatColor?:string;
+    public upholsteryColor?:string;
+    public trimType?:string;
+    
+
+    public displaySize?:string;
+    public displayHeadsUp?:boolean;
+    public autopilot?:boolean;
+
+    constructor(){}
+}
