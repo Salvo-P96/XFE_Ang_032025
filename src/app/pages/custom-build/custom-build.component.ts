a3823facd478d2c518a245bb4298a05f18a9f1a4
Salvo-P96 <salvop.6896@gmail.com>
2025-03-26 13:52:30 +0100

    custom build rotta da sisteamre

--- src/app/pages/custom-build/custom-build.component.ts
+++ src/app/pages/custom-build/custom-build.component.ts
@@ -1,39 +1,30 @@
 import { Component, inject, HostListener } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { VariableBinding } from '@angular/compiler';
+import { Customcar } from '../../../Models/CustomBuild';
 
 @Component({
   selector: 'app-custom-build',
   imports: [
-    MatButtonModule,
-    MatStepperModule,
     FormsModule,
     ReactiveFormsModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
+    MatSelectModule,
     MatInputModule,
-    MatMenuModule
   ],
   templateUrl: './custom-build.component.html',
   styleUrl: './custom-build.component.scss'
 })
 export class CustomBuildComponent {
 
-  private _formBuilder = inject(FormBuilder);
-
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  isLinear = false;
-
+  selectFormControl = new FormControl('', Validators.required);
   constructor(private router: Router, private route: ActivatedRoute) {}
     
   ngOnInit(): void {
@@ -45,24 +36,12 @@ export class CustomBuildComponent {
   }
 
   //-----------------------------------------------------
-  isMenuVisible = false;
-
   toggleMenu(): void {
-    this.isMenuVisible = !this.isMenuVisible;
+    const menu: HTMLElement|null = document.querySelector('.dropdown-menu');
+    menu?.classList.toggle('show');
+    console.log(menu)
   }
 
-  @HostListener('document:click', ['$event'])
-  closeMenuIfClickedOutside(event: MouseEvent): void {
-    const menu = document.querySelector('.dropdown-menu');
-    const button = document.querySelector('button');
-    
-    if (menu && !menu.contains(event.target as Node) && button && !button.contains(event.target as Node)) {
-      this.isMenuVisible = false;
-    }
-  }
-  //------------------------------------------------------
-  // select(v:VariableBinding, s:string):void{
-  //   const choice:HTMLElement|null= document.querySelector('choice');
-  //   let selectedChoice = choice ? choice.innerHTML : null;
-  // }
+  brand:string ="";
+
 }
