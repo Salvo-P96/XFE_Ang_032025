55db9e8183306f93ad062cdff3c0594aaea58af0
HORSA\luca.puglisi <luca.puglisi@horsa.it>
2025-04-09 12:55:25 +0200

    fix form generation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,41 +1,44 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
-    NgIf
-
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  storedusername:string="pippo"
-  storedpassword:string="0000"
+  storedusername: string = "pippo"
+  storedpassword: string = "0000"
 
-  loginForm:FormGroup;
+  loginForm: FormGroup;
 
-  constructor(){
-    this.loginForm=new FormGroup({
-      username: new FormControl(['',Validators.required]),
-      password: new FormControl(['',Validators.required]) 
+  //Aggiungere il FormBuilder nel costruttore ed utilizzarlo per creare il FormGroup
+  constructor(private fb: FormBuilder) {
+    //La funzione group() del FormBuilder è più indicata rispetto ad istanziare un nuovo FormGroup
+    // in modo da non dover instanziare anche i FormControl.
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     })
 
-    }
+  }
 
 
-    onSubmit(): void {
+  onSubmit(): void {
 
-      const username = this.loginForm.get('username')?.value;
-      const password = this.loginForm.get('password')?.value;
-  
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
 
-      if (username === this.storedusername && password === this.storedpassword) {
-        console.log("Access granted to:", username);
-      } else {
-        console.log("Access denied");
-      }
+
+    if (username === this.storedusername && password === this.storedpassword) {
+      console.log("Access granted to:", username);
+    } else {
+      console.log("Access denied");
     }
   }
+}
