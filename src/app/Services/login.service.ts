505a89b896fc6cf83695168782ccae39e34c8031
HORSA\luca.puglisi <luca.puglisi@horsa.it>
2025-04-09 15:42:05 +0200

    fix httpClient, p.s. leggi i commenti

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -10,6 +10,8 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
 
+  //Per poter recuperare il file locale ho dovuto modificare il file angular.json alle righe 29 e 90
+  //in modo da rendere disponibile la cartella assets per le chiamate http
   getUsers(): Observable<any[]> {
     return this.http.get<any[]>('assets/database/users.json');
   }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { LoginService } from '../../Services/login.service';
-import { HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-login',
-  standalone: true,
   imports: [
     NgIf,
     ReactiveFormsModule,
-    
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   storedusername: string = "pippo"
   storedpassword: string = "0000"
 
   loginForm: FormGroup;
-  users:{ username: string, password: string } [] = [];
 
-  constructor(private fb: FormBuilder,private loginService: LoginService) {
+  //Aggiungere il FormBuilder nel costruttore ed utilizzarlo per creare il FormGroup
+  //Aggiunto il LoginService
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    //La funzione group() del FormBuilder è più indicata rispetto ad istanziare un nuovo FormGroup
+    // in modo da non dover instanziare anche i FormControl.
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -31,21 +30,17 @@ export class LoginComponent implements OnInit {
 
   }
 
-  ngOnInit(): void {
-    this.loginService.getUsers().subscribe((data: { username: string, password: string }[]) => {
-      this.users = data;
-    });
+  //Effettuata chiamata al servizio
+  ngOnInit(){
+    this.loginService.getUsers().subscribe(res => console.log(res))
   }
-
-
   onSubmit(): void {
 
     const username = this.loginForm.get('username')?.value;
     const password = this.loginForm.get('password')?.value;
 
-    const accessGranted = this.users.find(u => u.username === username && u.password === password);
 
-    if (accessGranted) {
+    if (username === this.storedusername && password === this.storedpassword) {
       console.log("Access granted to:", username);
     } else {
       console.log("Access denied");
