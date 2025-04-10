d588c8006b132b49dabc69e786f36fe97f5999ca
Salvo-P96 <salvop.6896@gmail.com>
2025-04-10 14:58:12 +0200

    summary logic begun

--- src/app/pages/admin-home/admin-home.component.ts
+++ src/app/pages/admin-home/admin-home.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { isEmpty } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-admin-home',
+  imports: [NgIf
+   ],
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.scss']
 })
 export class AdminHomeComponent implements OnInit {
   userName: string = '';
-
+  summaryList:any=[5];
+  isListEmpty:boolean=true;
 
   ngOnInit(): void {
     const user = JSON.parse(localStorage.getItem('name') || '{}');
@@ -15,6 +20,16 @@ export class AdminHomeComponent implements OnInit {
       this.userName = user.name;
     } else {
       this.userName = 'Utente';
+    }
+  }
+  
+
+  //temporaneo, logica su schermo per ragionare
+  populateList():void{
+    if(this.summaryList.length==0){
+      this.isListEmpty=true
+    } else {
+      this.isListEmpty=false
     }
   }
 }
